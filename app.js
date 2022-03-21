97df258659a4b2c04e2e753abfdc9b6f970262aa
ununennium111 <isaac.cv123@gmail.com>
2022-03-20 21:57:44 -0300

    index.html created and added.
    * The file "index.html" has been created in the directory /public and added as a static file to "app.js".
    * This file contains the html frotend of the web.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const errorHandler = require('./middlewares/error-handler');
 const app = express();
 
 // Middlewares
+app.use(express.static('./public'));
 app.use(express.json());
 app.use(morgan('dev'));
 
